a72cb7426e4e8e19bc097c0eec92e26d658c3e2b
Mark Griffiths <mark@thebespokepixel.com>
2015-09-22 19:17:58 +0100

    Release v0.0.9

--- bin/trucolor.js
+++ bin/trucolor.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 "use strict";
 /*
- trucolor (v0.0.9-alpha.63)
+ trucolor (v0.0.9)
  24bit color tools for the command line
  */
 require( "../lib/cli");
--- src/lib/trucolor.coffee
+++ src/lib/trucolor.coffee
@@ -1,6 +1,6 @@
 'use strict'
 ###
- trucolor (v0.0.9-alpha.63)
+ trucolor (v0.0.9)
  24bit color tools for the command line
 ###
 
