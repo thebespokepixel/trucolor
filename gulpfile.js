b39946a5c7b7f2c84ec32faa43e436f64890d839
Mark Griffiths <mark@thebespokepixel.com>
2018-06-23 15:38:56 +0100

    Update package

--- gulpfile.js
+++ gulpfile.js
@@ -2,56 +2,56 @@
  │ trucolor gulp/cordial │
  ╰───────────────────────┴───────────────────────────────────────────────────── */
 
-const gulp = require('gulp')
-const cordial = require('@thebespokepixel/cordial')()
+// const gulp = require('gulp')
+// const cordial = require('@thebespokepixel/cordial')()
 
-// transpilation/formatting
-gulp.task('bundle', cordial.macro({
-	source: 'src/index.js'
-}).bundle())
+// // transpilation/formatting
+// gulp.task('bundle', cordial.macro({
+// 	source: 'src/index.js'
+// }).bundle())
 
-gulp.task('master', cordial.macro({
-	master: true,
-	source: 'src/index.js'
-}).bundle())
+// gulp.task('master', cordial.macro({
+// 	master: true,
+// 	source: 'src/index.js'
+// }).bundle())
 
-// Clean
-gulp.task('clean', cordial.shell({
-	source: ['bin/*', 'lib', 'npm-debug.*', './.nyc_output', './coverage']
-}).trash())
+// // Clean
+// gulp.task('clean', cordial.shell({
+// 	source: ['bin/*', 'lib', 'npm-debug.*', './.nyc_output', './coverage']
+// }).trash())
 
-// Docs
-gulp.task('docs', cordial.shell({
-	source: 'npm run doc-build'
-}).job())
+// // Docs
+// gulp.task('docs', cordial.shell({
+// 	source: 'npm run doc-build'
+// }).job())
 
-// ReadMe
-gulp.task('readme', cordial.shell({
-	source: 'npm run readme'
-}).job())
+// // ReadMe
+// gulp.task('readme', cordial.shell({
+// 	source: 'npm run readme'
+// }).job())
 
-gulp.task('cli', gulp.series(
-	cordial.format({
-		source: 'src/cli/index.js'
-	}).rollup.babel({
-		banner: '#! /usr/bin/env node',
-		dest: 'bin/trucolor'
-	}),
+// gulp.task('cli', gulp.series(
+// 	cordial.format({
+// 		source: 'src/cli/index.js'
+// 	}).rollup.babel({
+// 		banner: '#! /usr/bin/env node',
+// 		dest: 'bin/trucolor'
+// 	}),
 
-	cordial.shell().permissions({
-		mode: '755',
-		dest: 'bin/trucolor'
-	})
-))
+// 	cordial.shell().permissions({
+// 		mode: '755',
+// 		dest: 'bin/trucolor'
+// 	})
+// ))
 
-// Tests
-gulp.task('ava', cordial.test().ava(['test/*.js']))
-gulp.task('xo', cordial.test().xo(['src/**.js']))
-gulp.task('test', gulp.parallel('xo', 'ava'))
+// // Tests
+// gulp.task('ava', cordial.test().ava(['test/*.js']))
+// gulp.task('xo', cordial.test().xo(['src/**.js']))
+// gulp.task('test', gulp.parallel('xo', 'ava'))
 
-// Hooks
-gulp.task('start-release', gulp.series('reset', 'clean', gulp.parallel('master', 'cli'), 'readme'))
-gulp.task('post-flow-release-start', gulp.series('start-release', 'version-release', 'docs', 'commit'))
+// // Hooks
+// gulp.task('start-release', gulp.series('reset', 'clean', gulp.parallel('master', 'cli'), 'readme'))
+// gulp.task('post-flow-release-start', gulp.series('start-release', 'version-release', 'docs', 'commit'))
 
-// Default
-gulp.task('default', gulp.series('bump', 'clean', gulp.parallel('docs', 'bundle', 'cli', 'readme')))
+// // Default
+// gulp.task('default', gulp.series('bump', 'clean', gulp.parallel('docs', 'bundle', 'cli', 'readme')))
