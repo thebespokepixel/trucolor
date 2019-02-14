63f09b32bb66de74966668c2650794290686555b
Mark Griffiths <mark@thebespokepixel.com>
2019-02-14 18:48:30 +0000

    Update docs

--- index.mjs
+++ index.mjs
@@ -571,7 +571,7 @@ function _objectSpread(target) {
 }
 
 const name = "trucolor";
-const version = "0.7.3";
+const version = "0.7.4";
 const description = "TTY color toolkit supporting Truecolor (24bit RGB)";
 const author = "Mark Griffiths <mark@thebespokepixel.com> (http://thebespokepixel.com/)";
 const main = "index.js";
